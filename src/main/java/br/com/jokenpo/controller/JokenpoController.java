85e142fa9bd23bd970ac4e226e261535aca0ec63
Dev <vicentcdb@gmail.com>
2020-06-24 17:42:34 -0300

    player 3 validator

--- src/main/java/br/com/jokenpo/controller/JokenpoController.java
+++ src/main/java/br/com/jokenpo/controller/JokenpoController.java
@@ -15,47 +15,76 @@ import br.com.jokenpo.model.Rules;
 @RequestMapping("/jokenpoapi")
 public class JokenpoController {
 	
+	boolean Player3Present = false;
+	private Rules execHandPlayer3 = null;
+	private String handPlayerName3 = null;
+	
 	@PostMapping(value="/play", produces = MediaType.APPLICATION_JSON_VALUE)
 	private Player verificarJogada(@RequestBody ObjectNode data) {
 		
-		String handPlayer1 = data.get("player1").get(0).get("hand").asText();
-		String handPlayer1Name = data.get("player1").get(0).get("nome").asText();
+		String handPlayer1 = extUserInfo(data,"player1","hand");
+		String handPlayer1Name = extUserInfo(data,"player1","nome");
 		
-		String handPlayer2 = data.get("player2").get(0).get("hand").asText();
-		String handPlayer1Name2 = data.get("player2").get(0).get("nome").asText();
-		
-		System.out.println("NUMERO de PLAYERS:::: " + data.size());
+		String handPlayer2 = extUserInfo(data,"player2","hand");
+		String handPlayer2Name = extUserInfo(data,"player2","nome");
 		
 		//Setup Players
-		Player player1 = new Player();
-		Player player2 = new Player();
-		
-		if(data.size() == 3) {
-			Player player3 = new Player();
-		}
-		//################
+		Player player = new Player();
+		//Player player2 = new Player();
 		
 		Rules execHandPlayer1 = setHandPlayer(handPlayer1);
 		Rules execHandPlayer2 = setHandPlayer(handPlayer2);
-		
 			
-		if(execHandPlayer1.ganha(execHandPlayer2)) {
+		//Detect terceiro player
+		if(data.size() == 3) {
 			
-			player1.setHand(execHandPlayer1.toString());
-			player1.setNome(handPlayer1Name);
-			player1.setStatus("VENCEU");
+			System.out.println("DATA SIZE::::: " + data.size());
 			
-			return player1;
+			String handPlayer3 = data.get("player3").get(0).get("hand").asText();
+			handPlayerName3 = data.get("player3").get(0).get("nome").asText();
 			
+			execHandPlayer3 = setHandPlayer(handPlayer3);
+			Player3Present = true;
 		}else {
-			
-			player1.setHand(execHandPlayer2.toString());
-			player1.setNome(handPlayer1Name2);
-			player1.setStatus("VENCEU");
-			
-			return player1;
+			execHandPlayer3 = null;
 		}
+		//################
 		
+			
+		if(execHandPlayer1.ganha(execHandPlayer2) && execHandPlayer1.ganha(execHandPlayer3)) {			
+			
+			player.setHand(execHandPlayer1.toString());
+			player.setNome(handPlayer1Name);
+			player.setStatus("VENCEU");
+			
+			return player;
+			
+		}else{
+			
+			if(execHandPlayer2.ganha(execHandPlayer1) && execHandPlayer1.ganha(execHandPlayer3)) {
+				
+				player.setHand(execHandPlayer2.toString());
+				player.setNome(handPlayer2Name);
+				player.setStatus("VENCEU");
+				
+				return player;
+				
+			} else {
+				
+				if(execHandPlayer3.ganha(execHandPlayer2) && execHandPlayer3.ganha(execHandPlayer1)) {
+					
+					player.setHand(execHandPlayer3.toString());
+					player.setNome(handPlayerName3);
+					player.setStatus("VENCEU");
+					
+					return player;
+					}
+				
+			}
+			
+			return player;
+		} 
+
 }	
 	
 	private Rules setHandPlayer(String handPlayer) {
@@ -83,5 +112,10 @@ public class JokenpoController {
 		}
 		
 		return setHand;
+	}
+	
+	private String extUserInfo(ObjectNode data, String player, String campo) {
+		
+		return data.get(player).get(0).get(campo).asText();
 	}
 }
